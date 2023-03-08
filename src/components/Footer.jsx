626220a05e18ad590cb95b026a17c1d41b3e417f
Naeem Ullah <enaeemullah@gmail.com>
2023-03-08 23:26:30 +0500

    Structure Code
    Structure Code

---
+++ src/components/Footer.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import { styles } from '../styles';
+const Footer = () => {
+  return (
+    <h2
+      className={`${styles.paddingX} w-full flex items-center py-5 fixed top-0 z-20 bg-primary`}
+    >
+      {/* Hello */}
+    </h2>
+  );
+};
+
+export default Footer;
